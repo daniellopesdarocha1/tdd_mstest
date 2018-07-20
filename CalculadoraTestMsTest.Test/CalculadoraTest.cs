4a8b4dd1d47b39f2d46a9605760d5bf931deabb6
Daniel Rocha <daniellopesdarocha1@gmail.com>
2018-07-20 14:17:44 -0300

    Exemplo TDD MSTest

---
+++ CalculadoraTestMsTest.Test/CalculadoraTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculadoraTestMsTest.Test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        [TestMethod]
+        public void Calcular_Somar_1Mais2_Resultado3()
+        {
+            //Arrange
+            Calculadora.Calcular calcular = new Calculadora.Calcular();
+            decimal resultadoEsperado = 3;
+
+            //Act
+            decimal resultado = calcular.Somar(1, 2);
+
+            //Assert
+            Assert.AreEqual(resultado, resultadoEsperado);
+        }
+    }
+}
---
+++ CalculadoraTestMsTest.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculadoraTestMsTest.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculadoraTestMsTest.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b662f65-f1b4-41a4-8b26-5bcfa57e85ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
